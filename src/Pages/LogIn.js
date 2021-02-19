3dda244b32d9f42e09814e2f75df1a66c41a4c4b
Viktorija Garifova <kairotv21@gmail.com>
2021-02-19 16:15:47 +0000

    test

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -3,13 +3,8 @@ import React from 'react'
 function LogIn() {
     return (
         <div>
-<<<<<<< HEAD
             {/*Daniel*/}
             Login
-=======
-            //Daniel
-            //testing merge conflicts
->>>>>>> d763a2231fa2f69b803e45156ea47f007e23d4a4
         </div>
     )
 }
