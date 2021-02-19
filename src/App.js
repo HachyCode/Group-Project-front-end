257df4ca215f94889192a5c487b74282c935ee92
Viktorija Garifova <kairotv21@gmail.com>
2021-02-19 15:43:54 +0000

    Working navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/NavBar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Cateogries from './Pages/Cateogries';
+import LogIn from './Pages/LogIn';
+import POs from './Pages/POs';
+import POForm from './Pages/POForm';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+    <Navbar />
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/cateogries' component={Cateogries} />
+      <Route path='/logIn' component={LogIn} />
+      <Route path='/pos' component={POs} />
+      <Route path='/poForm' component={POForm} />
+    </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+} from './NavbarCSS';
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            {/*<NavLink to='/'>
+            <img src={require('../../images/logo.svg')} alt='logo' />
+            </NavLink>
+
+            <Bars />*/}
+
+            <NavMenu>
+                <NavLink to='/cateogries' activeStyle>
+                    Cateogries
+                </NavLink>
+
+                <NavLink to='/logIn' activeStyle>
+                    LogIn
+                </NavLink>
+
+                <NavLink to='/pos' activeStyle>
+                    POs
+                </NavLink>
+
+                <NavLink to='/poForm' activeStyle>
+                    POForm
+                </NavLink>
+            </NavMenu>
+
+            {/*<NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>*/}
+
+        </Nav>
+        </>
+    );
+};
+
+export default Navbar;
