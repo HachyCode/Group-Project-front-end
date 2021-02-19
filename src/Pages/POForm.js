8e154e4b6b4541fe0e53b1bba1f8a073c913f226
Viktorija Garifova <kairotv21@gmail.com>
2021-02-19 13:49:36 +0000

    Start

---
+++ src/Pages/POForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function POForm() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default POForm
---
+++ src/Pages/POs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function POs() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default POs
