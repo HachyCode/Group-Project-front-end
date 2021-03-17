8f77077c1def261b57d79d118761f858b6f56e8e
Coldani3 <p110117069@gmail.com>
2021-03-17 09:41:27 +0000

    Update server URL.

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
 	//put the IP of the running server in this
-	serverLocation: "http://localhost:4000" // /user/signup 
+	serverLocation: "http://134.122.25.193" // /user/signup 
 };
 
 export default Config;
