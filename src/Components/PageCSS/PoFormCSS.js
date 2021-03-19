50d6f149012ef7c9b3e092f36d686cb30a3273e8
Viktorija Garifova <kairotv21@gmail.com>
2021-03-19 14:04:41 +0000

    Adding top to PO Form page

--- src/Components/PageCSS/PoFormCSS.js
+++ src/Components/PageCSS/PoFormCSS.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
+export const TopMainBody = styled.div`
+    display: flex;
+    flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	margin: 2em 0;
+
+	@media screen and (max-width: 900px){
+		font-size: 0.8em;
+	}
+
+	@media screen and (max-width: 700px){
+		font-size: 0.7em;
+	}
+`;
+
+export const FilterTop = styled.div`
+	background: white;
+`;
+
 export const MainInfoBox = styled.div`
     display: flex;
     justify-content: space-between;
