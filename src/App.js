74fac4bdc9b4f6859eab91b0b87c14682ae0d721
Coldani3 <p110117069@gmail.com>
2021-03-24 20:01:46 +0000

    Navbar no longer renders if the user is not logged in.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,49 @@ import Categories from './Pages/Categories';
 import POs from './Pages/POs';
 import POForm from './Pages/POForm';
 import LogOff from './Pages/LogIn';
+import Config from './Config';
+import React from 'react';
+import {eventBus, RouterUpdate} from './EventBus';
 
-function App() {
-	return (
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-		<Router>
-			<Navbar/>
-			<Switch>
-				<Route path='/home' exact component={Home} />
-				<Route path='/categories' component={Categories} />
-				<Route path='/pos' component={POs} />
-				<Route path='/poForm' component={POForm} />
-				<Route path='/' component={LogOff} />
-			</Switch>
-		</Router>
+	componentDidMount = () => {
+		eventBus.on(RouterUpdate, this.update);
+	}
 
-	);
+	componentWillUnmount= () => {
+		eventBus.off(RouterUpdate, this.update);
+	}
+
+	update = () => {
+		this.setState({});
+	}
+
+	genNavBar = () => {
+		const draw = !!sessionStorage.getItem(Config.userTokenSession);
+		if (draw) {
+			return <Navbar/>;
+		}
+	}
+
+	render = () => {
+		return (
+			<Router>
+				{ this.genNavBar() }
+				<Switch>
+					<Route path='/home' exact component={Home} />
+					<Route path='/categories' component={Categories} />
+					<Route path='/pos' component={POs} />
+					<Route path='/poForm' component={POForm} />
+					<Route path='/' component={LogOff} />
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default App;
