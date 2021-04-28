8d6b8fbd51e37c08e24514ea6739dbb199ed74fd
Viktorija Garifova <kairotv21@gmail.com>
2021-04-28 10:16:47 +0100

    Po's add button CSS fix

--- src/Components/PageCSS/POsCSS.js
+++ src/Components/PageCSS/POsCSS.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 export const MainBody = styled.div`
     display: flex;
     flex-direction: column;
-	justify-content: center;
+	justify-content: top;
 	align-items: center;
 
 	@media screen and (max-width: 900px){
